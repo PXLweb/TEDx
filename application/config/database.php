c4aa23b80b7b3551b4583abe9f31395d011def5f
stefanie-r <Stefanie.Ramaekers@student.pxl.be>
2015-05-13 16:22:08 +0200

    aanpassen homepage en toevoegen afbeeldingen

--- application/config/database.php
+++ application/config/database.php
@@ -65,8 +65,8 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> 'mysql:host=localhost;dbname=tedx',
 	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => 'toor',
+	'username' => 'pxluser',
+	'password' => 'pxl',
 	'database' => 'tedx',
 	'dbdriver' => 'pdo',
 	'dbprefix' => '',
--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -17,7 +17,7 @@ class Home extends CI_Controller {
 
     public function index() {
         $this->load->view('layout_components/header', $this->viewDataHome);
-        $this->load->view('home');
+        $this->load->view('homepage/home');
         $this->load->view('layout_components/footer');
     }
 
