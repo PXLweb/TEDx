d519864267ee0237f9e04dc2667feb1d52bb11a5
stefanie-r <Stefanie.Ramaekers@student.pxl.be>
2015-05-14 13:53:48 +0200

    homepage terug toevoegen

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,27 +5,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Home extends CI_Controller {
 
     private $viewDataHome;
-    private $navData;
+    private $userManager;
 
     public function __construct() {
         parent::__construct();
         $this->load->model('managers/DataGenerator');
         $this->load->model('managers/UserManager');
-
-        $dataGenerator = new DataGenerator();
-        $this->viewDataHome = $dataGenerator->getViewData('home', 'nl');
-        $this->navData = $dataGenerator->getNavData('nl');
+        $this->viewDataHome = (new DataGenerator)->getViewData('home', 'nl');
+        $this->userManager = new UserManager();
     }
 
     public function index() {
         $this->load->view('layout_components/header', $this->viewDataHome);
-        $this->load->view('layout_components/navbar', $this->navData);
-        $this->load->view('home');
+        $this->load->view('homepage/home');
         $this->load->view('layout_components/footer');
     }
 
     public function login() {
-//        $userManager = new UserManager();
         $viewDataLogin = (new DataGenerator)->getViewData('login', 'nl');
         $this->load->view('login', $viewDataLogin);
     }
--- application/views/homepage/home.php
+++ application/views/homepage/home.php
@@ -1,3 +1,46 @@
+
+<div class="navbar-wrapper">
+    <div class="container">
+        <nav class="navbar navbar-inverse navbar-static-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#"><?php echo $lang->getBrandName(); ?></a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#"><?php echo $lang->getMenuHome(); ?></a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="#">Action</a></li>
+                                <li><a href="#">Another action</a></li>
+                                <li><a href="#">Something else here</a></li>
+                                <li class="divider"></li>
+                                <li class="dropdown-header">Nav header</li>
+                                <li><a href="#">Separated link</a></li>
+                                <li><a href="#">One more separated link</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#"><?php echo $lang->getMenuContact(); ?></a></li>
+                        <li><a href="#"><?php echo $lang->getMenuAbout(); ?></a></li>
+                        <li><a href="<?php echo site_url('login'); ?>"><?php echo $lang->getMenuLogin(); ?></a></li>
+                        <li><a href="<?php echo site_url('registreren'); ?>"><?php echo $lang->getMenuRegister(); ?></a></li>
+                        <li></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+    </div>
+</div>
+
+
 <!-- Carousel
 ================================================== -->
 <div id="myCarousel" class="carousel slide" data-ride="carousel">
@@ -9,32 +52,32 @@
     </ol>
     <div class="carousel-inner" role="listbox">
         <div class="item active">
-            <img class="first-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide">
+            <img class="first-slide" src="./assets/images/TEDxPXL.png" alt="First slide">
             <div class="container">
                 <div class="carousel-caption">
-                    <h1>Example headline.</h1>
+              <!--      <h1>Example headline.</h1>
                     <p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
-                    <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+                    <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p> -->
                 </div>
             </div>
         </div>
         <div class="item">
-            <img class="second-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide">
+            <img class="second-slide" src="./assets/images/176.jpg" alt="Second slide">
             <div class="container">
                 <div class="carousel-caption">
-                    <h1>Another example headline.</h1>
+                 <!--   <h1>Another example headline.</h1>
                     <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                    <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+                    <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>-->
                 </div>
             </div>
         </div>
         <div class="item">
-            <img class="third-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide">
+            <img class="third-slide" src="./assets/images/tedx_1.jpg" alt="Third slide">
             <div class="container">
                 <div class="carousel-caption">
-                    <h1>One more for good measure.</h1>
+                <!--   <h1>One more for good measure.</h1>
                     <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                    <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+                    <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>-->
                 </div>
             </div>
         </div>
@@ -59,21 +102,18 @@
     <!-- Three columns of text below the carousel -->
     <div class="row">
         <div class="col-lg-4">
-            <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-            <h2>Heading</h2>
-            <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+            <img class="img-circle" src="./assets/images/TEDxPXL.png" alt="Generic placeholder image" width="140" height="140">
+            <h2>TEDx</h2>
             <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
-            <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-            <h2>Heading</h2>
-            <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+            <img class="img-circle" src="./assets/images/TEDx_logo_RGB_3650.jpg" alt="Generic placeholder image" width="140" height="140">
+            <h2>TEDxPXL</h2>
             <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
-            <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-            <h2>Heading</h2>
-            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+            <img class="img-circle" src="./assets/images/logo_pxl_it.jpg" alt="Generic placeholder image" width="140" height="140">
+            <h2>PXL</h2>
             <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
     </div><!-- /.row -->
@@ -85,11 +125,15 @@
 
     <div class="row featurette">
         <div class="col-md-7">
-            <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
-            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+            <h2 class="featurette-heading"><span class="text-muted">TEDxPXL</span></h2>
+            <p class="lead">TEDxPXL is de programma van de PXL om mensen samen te brengen om een TED ervaring te delen. 
+            Deze site is toegewijd aan de evenementen die PXL organiseerd rond TEDx. 
+            Op deze site kunt u een oog houden op de toekomstige evenementen, de gegeven commentaren van een afgelopen event en nog veel meer vinden. 
+            
+           </p>
         </div>
         <div class="col-md-5">
-            <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+            <img class="featurette-image img-responsive center-block" src="./assets/images/TEDxPXL.png" alt="logo TEDxPXL">
         </div>
     </div>
 
@@ -97,11 +141,14 @@
 
     <div class="row featurette">
         <div class="col-md-7 col-md-push-5">
-            <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
-            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+            <h2 class="featurette-heading"><span class="text-muted">TEDx</span></h2>
+            <p class="lead">TED staat voor Technologie, Entertainment en Design. X staat voor onafhankelijke organisatie. 
+            Het is een non-profit organisatie in de VS, gericht op creatieve ideëen die het waard zijn over de wereld verspreid te worden. 
+            TED organiseerd jaarlijks een 4 dagen durende conferentie om over creatieve ideëen te spreken. Het lokt mensen van over de hele wereld. 
+            Het TED congres geeft wel richtlijnen voor het TEDx programma, maar de TEDx evenementen zijn zelf georganiseert.</p>
         </div>
         <div class="col-md-5 col-md-pull-7">
-            <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+            <img class="featurette-image img-responsive center-block" src="./assets/images/TEDx_logo_RGB_3650.jpg" alt="logo TEDX">
         </div>
     </div>
 
@@ -109,11 +156,14 @@
 
     <div class="row featurette">
         <div class="col-md-7">
-            <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
-            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+            <h2 class="featurette-heading"><span class="text-muted">PXL </br>De school met het netwerk</span></h2>
+            <p class="lead">Hogeschool PXL (PXL University College) is de hogeschool met het meest diverse onderwijsaanbod in Limburg.<br/>
+            We willen een voortrekkersrol spelen in het toepassen van nieuwe informatie- en communicatietechnologieën in het onderwijs. 
+            Vernieuwende technologieën ondersteunen ons competentiegericht onderwijs. 
+            Dit betekent dat het ontwikkelen van je bekwaamheden, zoals die in het beroepsleven nodig zijn, centraal staat.</p>
         </div>
         <div class="col-md-5">
-            <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+            <img class="featurette-image img-responsive center-block" src="./assets/images/logo_pxl_it.jpg" alt="logo pxl">
         </div>
     </div>
 
