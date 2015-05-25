6783fd3d0d9bf4b6c949fc40ddebc77567f91b16
Tom Martens <tommartens21@hotmail.com>
2015-05-25 17:26:12 +0200

    aanpassen buttons

--- application/views/events.php
+++ application/views/events.php
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
-
-<!DOCTYPE html>
-<html>
-    <head>
-        
-        <title><?php echo basename(__FILE__); ?></title>
-  <meta charset="utf-8">
- <link rel="stylesheet" href="<?php echo site_url('assets/css/events_admin.css'); ?>" />
-=======
 <div class="container"> 
     <div class="agenda">
         <?php echo $calendar; ?>
     </div>
->>>>>>> 49cc79755c2591e5757d6a9ad9c95867e8f14921
 
     <div class="events">
         <?php
@@ -26,40 +15,6 @@
     </div>
 </div>
 
-<<<<<<< HEAD
-    </head>
-    <body>  
-     <?php echo $calendar; ?>
- 
- <?php
-
-
-        
-       
-
-         
-          
-               foreach ($events as $event)
-                    {
-                        echo 'Name: '          . $event['event_name']       . '<br />';
-                        echo 'Speaker: '        . $event['speaker']         . '<br />';
-                        echo 'Location: ' . $event['location']  . '<br />';
-                        echo 'Date: ' . $event['date_time']  . '<br /><br />';
-                    }
-                
-            
-            ?>
-
-=======
-
->>>>>>> 49cc79755c2591e5757d6a9ad9c95867e8f14921
 
 
 
-<<<<<<< HEAD
-
-    </body>
-</html>
-
-=======
->>>>>>> 49cc79755c2591e5757d6a9ad9c95867e8f14921
